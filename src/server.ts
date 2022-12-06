901b5b1370f32b6281a1539b01eef310f877390b
bentoluizv <bentoluizv@gmail.com>
2022-12-06 08:28:26 -0300

    chore: initial commit

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from "express";
+
+const PORT = process.env.PORT || 3000;
+const server = express();
+
+server.get("/", (req, res) => {
+  res.send("Em desenvolvimento!");
+});
+
+server.listen(PORT, () =>
+  console.log(`Server is running on ${PORT}, ${Date.now()}`)
+);
