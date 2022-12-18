f09107a9b52ca06cd5cd7fbabb13fa61749ea35d
bentoluizv <bentoluizv@gmail.com>
2022-12-18 20:32:13 -0300

    feat: api post method implementation

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,41 @@
+import { PrismaClient } from "@prisma/client";
 import express from "express";
+import { ShoppingListRepository } from "./data/repositories/ShoppingListRepository";
+import { ShoppingList } from "./domain/ShoppingList";
+import { ShoppingListItem } from "./domain/ShoppingListItem";
+import { ShoppingListService } from "./services/ShoppingListService";
+import { Status } from "./utils/types";
 
 const PORT = process.env.PORT || 3000;
 const server = express();
 
+const DB = new PrismaClient();
+
+const repository = new ShoppingListRepository(DB);
+const service = new ShoppingListService(repository);
+
+server.use("/", express.json());
 server.get("/", (_, res) => {
   res.send("Em desenvolvimento!");
+});
+2;
+server.post("/list", async (req, res) => {
+  const response: {
+    id: string;
+    status: Status;
+    items: Array<ShoppingListItem>;
+  } = req.body;
+
+  const list = new ShoppingList({ id: response.id, status: response.status });
+
+  for (const item of response.items) {
+    list.addItem(item);
+  }
+
+  const savedData = await service.createNewList(list);
+  const { id, createdAt, status, items } = savedData;
+  const data = { id, createdAt, status, items: Object.fromEntries(items) };
+  res.status(201).json(data);
 });
 
 server.listen(PORT, () =>
