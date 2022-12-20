0d8ac30112ca855c58a53224ae15dfe080311a6f
bentoluizv <bentoluizv@gmail.com>
2022-12-20 13:12:06 -0300

    chore: API integration test

--- tests/integration/API.test.ts
+++ tests/integration/API.test.ts
@@ -1,11 +1,13 @@
 import axios from "axios";
+import { Status } from "../../src/utils/types";
+
 describe("API Test", () => {
   it("Should GET / index route", async () => {
     const response = await axios.get("http://localhost:3000/");
     expect(response.status).toBe(200);
   });
 
-  it("Should POST /list", async () => {
+  it("Should POST /list and save a list", async () => {
     const data = {
       id: "fe57fdc2-6740-4c61-9987-1sad7c703k8y",
       status: "Open",
@@ -19,4 +21,26 @@ describe("API Test", () => {
     expect(response.status).toBe(201);
     expect(response.data).toMatchObject<typeof data>;
   });
+
+  it("Should GET /list/:id route and get a list by id", async () => {
+    const response = await axios.get(
+      "http://localhost:3000/list/fe57fdc2-6740-4c61-9987-1sad7c703k8y"
+    );
+
+    expect(response.status).toBe(201);
+    expect(response.data.id).toBe("fe57fdc2-6740-4c61-9987-1sad7c703k8y");
+    expect(response.data.items[0][1]).toStrictEqual({
+      name: "Café",
+      quantity: 2,
+    });
+  });
+
+  it("Should DELETE /list/:id route and delete a list by id", async () => {
+    const response = await axios.delete(
+      "http://localhost:3000/list/fe57fdc2-6740-4c61-9987-1sad7c703k8y"
+    );
+
+    expect(response.status).toBe(204);
+  });
 });
+
